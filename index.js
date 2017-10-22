24d3dbefe08e5c0a95f894e83ebd6ddcdf8b229f
Laura Slor <slorlaura@gmail.com>
2017-10-22 20:52:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(object, key, value) {
-return Object.assign({}, object, { [key]: value})
-return object
+var updateObjectWithKeyAndValue = function(object, key, value) {
+  return object.assign({}, object {[key]: value})
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-object[key]=value
-return object
-}
-
-function deleteFromObjectByKey(object,key) {
-const newObj= Object.assign ({}, object)
-delete newObj[key]
-return newObj
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
-return object
-}
